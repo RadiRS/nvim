993e51c119dc25726dffcf943280bcba125daa84
Radi <radi.rusadi.s@gmail.com>
2022-10-08 15:42:18 +0700

    Installed packer for package manager and setup new themes

---
+++ after/plugin/farout.rc.lua
@@ -0,0 +1,12 @@
+vim.opt.termguicolors = true
+vim.cmd("colorscheme farout")
+
+-- local status, n = pcall(require, "frout")
+-- if (not status) then 
+--  print('theme not found')
+--  return 
+-- end
+
+-- n.setup({
+--  commit_italics = true
+-- })
