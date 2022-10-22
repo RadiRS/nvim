7c3582224ba4d85744e70d90705472b6af921903
Radi <radi.rusadi.s@gmail.com>
2022-10-23 01:54:58 +0700

    Installed & configure indent-blankline

---
+++ after/plugin/indent.rc.lua
@@ -0,0 +1,11 @@
+local status, indent = pcall(require, 'indent_blankline')
+if (not status) then return end
+
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+
+indent.setup {
+  space_char_blankline = " ",
+  show_current_context = true,
+  show_current_context_start = true,
+}
