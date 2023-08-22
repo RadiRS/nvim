005d5be545c702b96848f0a53391999665e02795
Radi <radi.rusadi.s@gmail.com>
2023-08-22 19:59:40 +0700

    refactor: start refresh

--- after/plugin/comment.rc.lua
+++
@@ -1,8 +0,0 @@
-local status, comment = pcall(require, "Comment")
-if (not status) then return end
-local status1, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
-if (not status1) then return end
-
-comment.setup {
-  pre_hook = commentstring.create_pre_hook()
-}
--- after/plugin/package-info.rc.lua
+++
@@ -1,6 +0,0 @@
-local status, package_info = pcall(require, 'package-info')
-if (not status) then return end
-
-package_info.setup({
-  hide_up_to_date = true
-})
