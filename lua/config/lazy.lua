658c9c6ecad31454a4f990d43267d25e28423d32
Radi Rusadi <radi.rusadi.s@gmail.com>
2024-05-26 19:24:24 +0700

    chore: add plugins for indent scope and transparent background

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,6 +22,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.luasnip" },
+    { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     -- import/override with your plugins
     { import = "plugins" },
   },
