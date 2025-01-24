cf0385129706b8e6381e271cf33907770af3371f
Radi Rusadi <radi.rusadi.s@gmail.com>
2025-01-25 06:42:09 +0700

    feat: add neovimtest adapter and configuration
    this config use for running javacript test file

---
+++ lua/plugins/test.lua
@@ -0,0 +1,13 @@
+return {
+  { "nvim-neotest/neotest-jest" },
+  { "marilari88/neotest-vitest" },
+  {
+    "nvim-neotest/neotest",
+    opts = {
+      adapters = {
+        "neotest-jest",
+        "neotest-vitest",
+      },
+    },
+  },
+}
