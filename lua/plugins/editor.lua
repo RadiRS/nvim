064fcc1d0c788b5b690c8afcad6137c5acd3b832
Radi Rusadi <radi.rusadi.s@gmail.com>
2025-01-28 07:04:46 +0700

    feat: enable smear cursor animation extra

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -21,4 +21,17 @@ return {
       { ";e", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Workspace Diagnostics" },
     },
   },
+
+  -- Cursor animation Config
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      cursor_color = "none",
+      legacy_computing_symbols_support = true,
+      stiffness = 0.8,
+      trailing_stiffness = 0.5,
+      distance_stop_animating = 0.5,
+      hide_target_hack = false,
+    },
+  },
 }
